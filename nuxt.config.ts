ae5e42cc656cdae75748979bba79b0f3ee26d3da
renataleal335 <renata.leal@melhorenvio.com>
2023-10-17 18:40:42 -0300

    feat: pwa settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    registerType: 'autoUpdate',
+    injectRegister: 'auto',
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html}']
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Melhor Voucher',
+      short_name: 'MelhorVoucher',
+      description: 'Acompanhe sua pontuação na Melhor Envio',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  }
 })
