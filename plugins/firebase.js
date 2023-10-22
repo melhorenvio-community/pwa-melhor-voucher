295339e0ab3531ee4c29f5f4f465f75f9aaf09e2
renataleal335 <renata.leal@melhorenvio.com>
2023-10-22 19:18:41 -0300

    feat: add user state and refactor firebase config file

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,16 +1,15 @@
 
 import { initializeApp } from 'firebase/app';
+import { initUser } from '../composables/useFirebaseHandleUser'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const config = useRuntimeConfig()
-  console.log('config', config)
+
   const firebaseConfig = {
     apiKey: config.public.FIREBASE_API_KEY,
   };
 
   const app = initializeApp(firebaseConfig);
 
-  console.log(app)
+  initUser();
 });
-
-
