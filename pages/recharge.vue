dad87d2c7764dc36bbe8f9c3244e82d9bbe05a70
Helton Silva <helton.silva@melhorenvio.com>
2023-11-01 17:57:24 -0300

    feat: page recharge

---
+++ pages/recharge.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="px-5">
+    <p class="font-bold pt-4">Recarga de pontos</p>
+
+    <div class="flex gap-2">
+      <P>{{ text }}</P>
+
+      <img 
+        class="cursor-pointer" 
+        src="~/assets/icons/audio.svg" 
+        alt="audio" 
+        @click="play()"
+      />
+    </div>
+    
+    
+    <MEButton @click="start">Câmera</MEButton>
+  </div>
+</template>
+
+<script setup>
+import { MEButton } from  '@melhorenvio/unbox';
+import { useSpeechSynthesis } from '@vueuse/core';
+
+const scanner = ref(null);
+const voice = ref(undefined);
+const text = ref('Coloque a câmera na direção do QRCode');
+
+const speech = useSpeechSynthesis(text, {
+  lang: 'pt-BR',
+  pitch: 1,
+  rate: 1,
+  volume: 1,
+  voice,
+});
+
+function play() {
+  speech.speak();
+}
+
+definePageMeta({
+ middleware: ['auth']
+});
+</script>
