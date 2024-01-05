7c9e1a0834f84309af8abac107aea05bae229093
Helton Silva <helton.silva@melhorenvio.com>
2024-01-05 15:07:21 -0300

    fix: add path public

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   },
   preset,
   images: [
-    'public/logo.svg',
-    'public/homeIllust.svg',
-    'public/logo-melhor-voucher.svg'
+    'public/icons/logo.svg',
+    'public/icons/homeIllust.svg',
+    'public/icons/logo-melhor-voucher.svg'
   ],
   maskable: {
     sizes: [512]
