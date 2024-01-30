cb49d4139e31708633b6d17c7fe27d1c9cf76d75
renataleal335 <renata.leal@melhorenvio.com>
2024-01-30 20:21:39 -0300

    feat: settings dev mode config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const isPWADevMode = process.env.VITE_PWA_DEV_MODE === 'true';
+
 export default defineNuxtConfig({
   app: {
     buildAssetsDir: 'public/',
@@ -48,7 +50,7 @@ export default defineNuxtConfig({
       ],
     },
     devOptions: {
-      enabled: true,
+      enabled: isPWADevMode,
       type:'module'
     },
     client: {
