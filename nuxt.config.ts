d205c58cc54c7b0dee1093fd6794407fb7cfc825
Helton Silva <helton.silva@melhorenvio.com>
2024-03-11 10:41:52 -0300

    fix: test offline app caches

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,10 +76,10 @@ export default defineNuxtConfig({
       display: 'standalone',
     },
     workbox: {
-      globPatterns: ['**/*.html'],
+         globPatterns: ['**/*.{js,html}']
     },
     injectManifest: {
-      globPatterns: ['**/*.html'],
+         globPatterns: ['**/*.{js,html}']
     },
     client: {
       installPrompt: true
