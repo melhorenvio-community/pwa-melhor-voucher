d01ea4a6a81a4d65b416b8cff0ede0e195193d0e
Helton Silva <helton.silva@melhorenvio.com>
2024-03-15 12:13:30 -0300

    test: test in production

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,54 +1,125 @@
 /// <reference lib="WebWorker" />
 /// <reference types="vite/client" />
+import { cacheNames, clientsClaim } from 'workbox-core'
+import { registerRoute, setCatchHandler, setDefaultHandler } from 'workbox-routing'
+import type { StrategyHandler } from 'workbox-strategies'
+import {
+  NetworkFirst,
+  NetworkOnly,
+  Strategy
+} from 'workbox-strategies'
+import type { ManifestEntry } from 'workbox-build'
 
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { CacheFirst } from 'workbox-strategies';
-import { ExpirationPlugin } from 'workbox-expiration';
-import { clientsClaim } from 'workbox-core'
+declare let self: ServiceWorkerGlobalScope
+declare type ExtendableEvent = any
 
-declare let self: ServiceWorkerGlobalScope;
+const data = {
+  race: false,
+  debug: false,
+  credentials: 'same-origin',
+  networkTimeoutSeconds: 0,
+  fallback: 'index.html'
+}
 
-precacheAndRoute(self.__WB_MANIFEST);
+const cacheName = cacheNames.runtime
+
+function buildStrategy(): Strategy {
+  if (race) {
+    class CacheNetworkRace extends Strategy {
+      _handle(request: Request, handler: StrategyHandler): Promise<Response | undefined> {
+        const fetchAndCachePutDone: Promise<Response> = handler.fetchAndCachePut(request)
+        const cacheMatchDone: Promise<Response | undefined> = handler.cacheMatch(request)
+
+        return new Promise((resolve, reject) => {
+          fetchAndCachePutDone.then(resolve).catch((e) => {
+            if (debug)
+              console.log(`Cannot fetch resource: ${request.url}`, e)
+          })
+          cacheMatchDone.then(response => response && resolve(response))
+
+          // Reject if both network and cache error or find no response.
+          Promise.allSettled([fetchAndCachePutDone, cacheMatchDone]).then((results) => {
+            const [fetchAndCachePutResult, cacheMatchResult] = results
+            if (fetchAndCachePutResult.status === 'rejected' && !cacheMatchResult.value)
+              reject(fetchAndCachePutResult.reason)
+          })
+        })
+      }
+    }
+    return new CacheNetworkRace()
+  }
+  else {
+    if (networkTimeoutSeconds > 0)
+      return new NetworkFirst({ cacheName, networkTimeoutSeconds })
+    else
+      return new NetworkFirst({ cacheName })
+  }
+}
+
+const manifest = self.__WB_MANIFEST as Array<ManifestEntry>
+
+const cacheEntries: RequestInfo[] = []
+
+const manifestURLs = manifest.map(
+  (entry) => {
+    const url = new URL(entry.url, self.location)
+    cacheEntries.push(new Request(url.href, {
+      credentials: credentials as any
+    }))
+    return url.href
+  }
+)
+
+self.addEventListener('install', (event: ExtendableEvent) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheEntries)
+    })
+  )
+})
+
+self.addEventListener('activate', (event: ExtendableEvent) => {
+  // - clean up outdated runtime cache
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      // clean up those who are not listed in manifestURLs
+      cache.keys().then((keys) => {
+        keys.forEach((request) => {
+          debug && console.log(`Checking cache entry to be removed: ${request.url}`)
+          if (!manifestURLs.includes(request.url)) {
+            cache.delete(request).then((deleted) => {
+              if (debug) {
+                if (deleted)
+                  console.log(`Precached data removed: ${request.url || request}`)
+                else
+                  console.log(`No precache found: ${request.url || request}`)
+              }
+            })
+          }
+        })
+      })
+    })
+  )
+})
 
 registerRoute(
-  ({ request }) => request.destination === 'style' || request.destination === 'document',
-  new CacheFirst({
-    cacheName: 'css-html-cache',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 20,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+  ({ url }) => manifestURLs.includes(url.href),
+  buildStrategy()
+)
 
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+setDefaultHandler(new NetworkOnly())
 
-const customHandler = createHandlerBoundToURL('/offline.html');
+// fallback to app-shell for document request
+setCatchHandler(({ event }): Promise<Response> => {
+  switch (event.request.destination) {
+    case 'document':
+      return caches.match(fallback).then((r) => {
+        return r ? Promise.resolve(r) : Promise.resolve(Response.error())
+      })
+    default:
+      return Promise.resolve(Response.error())
+  }
+})
 
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/login'),
-  ({ event }) => customHandler.handle({ event })
-);
-
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/register'),
-  ({ event }) => customHandler.handle({ event })
-);
-
-self.skipWaiting();
-clientsClaim();
-
+self.skipWaiting()
+clientsClaim()
